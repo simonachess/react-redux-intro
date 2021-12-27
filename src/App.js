778d6374b92e4e0281a8ad7cdcafcbdee4a8326f
simonachess <simonachess@gmail.com>
2021-12-27 20:54:21 +0200

    tasks list, add new task modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import AddTodo from './components/todo/AddTodo';
+import TopNav from './nav/TopNav';
+import TodoList from './components/todo/TodoList';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>Hello</p>
+    <div className="container">
+      <div className='row'>
+        <div className='col-md-7 m-auto'>
+          <TopNav />
+          <AddTodo />
+          <TodoList />
+        </div>
+      </div>
     </div>
   );
 }
